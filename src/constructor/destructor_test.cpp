7f108cd31e31380f819246d0eb8007d65c882130
HZ-Cyq <1369509439@qq.com>
2026-03-23 18:00:53 +0800

    暂提交

---
+++ src/constructor/destructor_test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+class A {
+public:
+    A(): data(new int[10]) {
+        cout << this << " default constructor" << endl;
+    }
+    ~A() {
+        cout << this << " destructor" << endl;
+        delete[] data;
+    }
+private:
+    int *data;    
+};
+
+int main() {
+    cout << "hello world2" << endl;
+    A a, *pa = new A();
+    cout << "a: " << &a << endl;
+    cout << "pa: " << pa << endl;
+    delete pa;
+    return 0;
+}
